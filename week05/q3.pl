9e4406b457dd65029b6c527c7337290955756bc6
mytluo <melissaluo@Melissas-MacBook-Pro.local>
2016-07-03 00:17:30 -0700

    corrected q3 to cgi format

--- week05/q3.pl
+++ week05/q3.pl
@@ -1,7 +1,28 @@
 #!/usr/bin/perl
 use strict;
 use warnings;
+use CGI (':standard');
 
+my $title = "Random Sequence Generator";
+print header,
+	start_html($title),
+	h1($title);
+
+if(param('submit')) {
+		my $type = param('type');
+		print("type", $type);
+		print p(random_sequence($type));
+		hr();
+}
+
+my $url = url();
+print start_form(-method => 'GET', action => $url),
+p("Choose sequence type to generate: " . radio_group(-name => 'type',
+													-values => ['DNA', 'Protein']));
+p(submit(-name => 'submit', value => 'Submit')),
+end_form();
+end_html();
+=
 # prompt for type of sequence desired
 sub prompt_type {
 	print "Please enter 'DNA' or 'protein' to generate desired sequence, or any other key to exit: ";
@@ -16,6 +37,7 @@ while ($seqtype =~ /^[DdPp]/) { # if user input starts with a D or P
 	random_sequence($seqtype);  # call on subroutine random_sequence
 	$seqtype = prompt_type();	# prompt user again and store results 
 }
+=cut
 
 sub random_sequence {
 	my @DNA = qw / A C G T /;
